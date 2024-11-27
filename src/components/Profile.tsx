2cbdbee51b29614cd2ecd825ac8337b0a680bddf
Thony Ivan Briza <brizathonyivand@gmail.com>
2024-11-27 19:21:24 +0800

    Update

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,31 +1,44 @@
+"use client";
+
 import { BsLinkedin } from "react-icons/bs";
 import { FaGithubSquare } from "react-icons/fa";
 import Image from 'next/image';
 
 export default function Profile() {
-    return (
-      <div className="flex flex-col md:flex-row w-screen h-full">
-        {/* Div 1: Profile Section */}
-        <div className="bg-[#0a0a0a] w-full md:w-1/2 h-1/2 md:h-full flex flex-col justify-center items-center p-8 text-center lg:mt-20 2xl:mt-48">
-          <h1 className="text-white text-4xl md:text-5xl font-bold mb-4">
-            HI, I AM <br></br><span className="text-[#b3ff00]">Thony Ivan Briza.</span>
-          </h1>
-          <p className="text-gray-400 text-lg md:text-xl lg:ml-14">
+  const handleSmoothScroll = (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+    event.preventDefault();
+    const targetId = event.currentTarget.getAttribute('href')?.slice(1);
+    if (targetId) {
+      const targetElement = document.getElementById(targetId);
+      if (targetElement) {
+        targetElement.scrollIntoView({ behavior: 'smooth' });
+      }
+    }
+  };
+
+  return (
+    <div className="flex flex-col md:flex-row w-screen h-full">
+      {/* Div 1: Profile Section */}
+      <div className="bg-[#0a0a0a] w-full md:w-1/2 h-1/2 md:h-full flex flex-col justify-center items-center p-8 text-center lg:mt-20 2xl:mt-48">
+        <h1 className="text-white text-4xl md:text-5xl font-bold mb-4">
+          HI, I AM <br></br><span className="text-[#b3ff00]">Thony Ivan Briza.</span>
+        </h1>
+        <p className="text-gray-400 text-lg md:text-xl lg:ml-14">
           I am a passionate front-end developer dedicated to creating 
           user-friendly and responsive websites. This portfolio was built using Next.js.
-          </p>
-          <div className="flex mt-8">
-            <button className="bg-[#b3ff00] text-black font-semibold py-2 px-4 rounded-full hover:bg-[#a0e600]">
-              Contact Me
-            </button>
-            <a target="_blank" href="https://www.linkedin.com/in/thony-ivan-briza-58325a244/" className="p-3">
-             <BsLinkedin size={40} />
-            </a>
-            <a target="_blank" href="https://github.com/Thony1412" className="p-3">
-             <FaGithubSquare size={40} />
-            </a>
-          </div>
+        </p>
+        <div className="flex mt-8">
+          <a href="#contacts" onClick={handleSmoothScroll} className="bg-[#b3ff00] text-black font-semibold py-5 px-4 rounded-full hover:bg-[#a0e600]">
+            Contact Me
+          </a>
+          <a target="_blank" href="https://www.linkedin.com/in/thony-ivan-briza-58325a244/" className="p-3">
+            <BsLinkedin size={40} />
+          </a>
+          <a target="_blank" href="https://github.com/Thony1412" className="p-3">
+            <FaGithubSquare size={40} />
+          </a>
         </div>
+      </div>
   
         {/* Div 2: Profile Picture */}
         <div className="bg-[#0a0a0a] w-full md:w-1/2 h-1/2 md:h-full flex justify-center p-4 ">
